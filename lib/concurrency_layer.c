0ca647c6c0301d1a86ec14c3f7d3d67c709a4a29
Mathias <mathias.amarillolemos@gmail.com>
2019-05-04 19:05:35 +0200

    Used the exercises algorithm to switch between trheads Broker->Operation_Exec->Broker->etc

--- lib/concurrency_layer.c
+++ lib/concurrency_layer.c
@@ -10,29 +10,14 @@ pthread_cond_t cond2;
 int QueueBusy = 0; // this will determine wather the queue is availabe for the reader to read. 
 // int number of readers? 
 
-// initialization of the mutex and condition variables
-void init_concurrency_mechanisms(){
-    pthread_mutex_init(&lock, NULL);
-    pthread_cond_init(&cond, NULL);
-    pthread_cond_init(&cond2, NULL);
-    printf("------>init concurrency mechanisns ok \n");
-}
+/* Extract information from data received in the pointer void * args -> OK
+Create the iterator on the batch file (new_iterator)
+While there are pending file operations
+Read a new operation from the file with the iterator (next_operation)
+Create a new operation with the information returned by the file (new_operation)
+Queue the new operation in the operation queue (enqueue_operation) 
+Destroy the iterator (destroy_iterator) */
 
-// Deallocate memory for the mutex and condition variables 
-void destroy_concurrency_mechanisms(){
-    pthread_mutex_destroy(&lock);
-    pthread_cond_destroy(&cond);
-    pthread_cond_destroy(&cond2);
-    printf("------>destroy concurrency mechanisns ok \n");
-}
-
-// Extract information from data received in the pointer void * args 
-// Create the iterator on the batch file (new_iterator)
-// While there are pending file operations
-// Read a new operation from the file with the iterator (next_operation)
-// Create a new operation with the information returned by the file (new_operation)
-// Queue the new operation in the operation queue (enqueue_operation) 
-// Destroy the iterator (destroy_iterator)
 void* broker(void* args){
 
     // we declare the main data structures that the broker need to interact with
@@ -49,44 +34,51 @@ void* broker(void* args){
 
     //Create an iterator
     if((_iterator = new_iterator(broker_batch))!= NULL){
-        printf("------->SUCCESS ITERATOR\n"); // DEBUG ONLY
-        // this retruns the muiber of successfull arguments manipulated through fscanf
-        operation_check = next_operation(_iterator, _operation->id, &_operation->type, &_operation->num_shares, &_operation->share_price);
+       
+        // this retruns the number of successfull arguments manipulated through fscanf 
+        operation_check = next_operation( _iterator, _operation->id, &_operation->type, 
+                                            &_operation->num_shares, &_operation->share_price);
 
-        // we keep reading the batch file until we reach the end of the file
-        while(operation_check != EOF){ 
-            printf("------->OPEARTION CHECK--->%d\n", operation_check);
-            printf("------->OPEARTION CHECK--->%s - %d- %d-  %d\n", _operation->id, _operation->type, _operation->num_shares, _operation->share_price);
-
-            new_operation(_newOperation, _operation->id, _operation->type, _operation->num_shares, _operation->share_price);
-            printf("------->NEW OPEARTION CHECK--->%s - %d- %d-  %d\n", _newOperation->id, _newOperation->type, _newOperation->num_shares, _newOperation->share_price);
+        while(operation_check != EOF){ // we keep reading the batch file until we reach the end of the file
+            new_operation(  _newOperation, _operation->id, _operation->type, 
+                            _operation->num_shares, _operation->share_price);
             
-            //*****************************+
-            //*****************************+ CRITICAL AREA START
-            //*****************************+
+            //****************************************************************************************+  CRITICAL AREA START
             pthread_mutex_lock(&lock);
-
+            
             // we wait while the queue is full. 
-            while (operations_queue_full(_stock_marquet->stock_operations) == 1){
-                printf("-------->operations_queue_full\n"); // this means that the queue is full 
+            while (/*operations_queue_full(_stock_marquet->stock_operations) == 1 && */QueueBusy == 1){
                 pthread_cond_wait(&cond,&lock);
+                printf("--------_>BROKER WAITING:...........");
             }
                 
-            if((enqueue_operation(_stock_marquet->stock_operations, _newOperation))<0)
-                return -1; // if it fails, we interrupt the program
-            sleep(2); // DEBUG ONLY
             
+            printf("\n\n--------------------------------------_>BROKER STARTS\n\n");    
+            if((enqueue_operation(_stock_marquet->stock_operations, _newOperation))<0)
+                return -1;
+            sleep(2); // DEBUG ONLY 
+            QueueBusy = 1;
             pthread_cond_signal(&cond);
-            pthread_cond_signal(&cond2);
+            //pthread_cond_signal(&cond2);
             pthread_mutex_unlock(&lock);
-            //*****************************+
-            //*****************************+ CRITICAL AREA FINISH
-            //*****************************+
-            operation_check =  next_operation(_iterator, _operation->id, &_operation->type, &_operation->num_shares, &_operation->share_price);
+            //****************************************************************************************+ CRITICAL AREA FINISH
+            
+            operation_check =  next_operation(  _iterator, _operation->id, &_operation->type, 
+                                                &_operation->num_shares, &_operation->share_price);
         }
     }
     destroy_iterator(_iterator);
 }
+
+
+
+
+
+
+
+
+
+
 // Extract information from data received in the pointer void * args 
 // While the exit flag is not active
 // Dequeue operation from the operations queue (dequeue_operation) 
@@ -103,44 +95,35 @@ void* operation_executer(void* args){
     int operation_check;
     int proc_operation;
 
-    //*****************************+
-    //*****************************+ CRITICAL AREA START
-    //*****************************+
-    pthread_mutex_lock(_operationExec->exit_mutex);
+    //****************************************************************************************+ CRITICAL ARE START
+  
+    //pthread_mutex_lock(_operationExec->exit_mutex);
 
-    while(*exit_OpExe != 1){
-        pthread_mutex_unlock(_operationExec->exit_mutex);
+    while(*exit_OpExe != 1){ // while tehe xit flag is not active
+
+        //pthread_mutex_unlock(_operationExec->exit_mutex);
         pthread_mutex_lock(&lock);
 
-        while(operations_queue_empty(_stock_marquetEx->stock_operations) == 1){
-            pthread_cond_wait(&cond2, &lock);
+        while(/*operations_queue_empty(_stock_marquetEx->stock_operations) == 1 && */QueueBusy == 0){
+            pthread_cond_wait(&cond, &lock);
         }
 
         operation_check = dequeue_operation(_stock_marquetEx->stock_operations, _operation);
-        //Check
-        if(operation_check == -1){
-            perror("Error in dequeue_operation()");
-            exit(-1);
-        }
         proc_operation =  process_operation(_stock_marquetEx, _operation);
-        //Check
-        if(proc_operation == -1){
-            perror("Error in process_operation()");
-            exit(-1);
-        }
+
+        printf("\n\n--------------------------------------_>OPETARION EXECUTER\n\n");
         sleep(2);
-        //*****************************+
-        //*****************************+ CRITICAL AREA FINISH
-        //*****************************+
+        QueueBusy = 0; 
         pthread_cond_signal(&cond);
-        pthread_cond_signal(&cond2);
+        //pthread_cond_signal(&cond2);
         pthread_mutex_unlock(&lock);
-        pthread_mutex_lock(_operationExec->exit_mutex);
+        //pthread_mutex_lock(_operationExec->exit_mutex);
+        //****************************************************************************************+ CRITICAL AREA FINISH
     }
-    pthread_mutex_unlock(_operationExec->exit_mutex);
+    //pthread_mutex_unlock(_operationExec->exit_mutex);
     
-        //When the flag exit is active, we have to close the Op_Ex,
-      //Before closing, we need to process all operations remaining in the queue
+    //When the flag exit is active, we have to close the Op_Ex,
+    //Before closing, we need to process all operations remaining in the queue
     while(operations_queue_empty(_stock_marquetEx->stock_operations) == 0){
         pthread_mutex_lock(&lock);
         dequeue_operation(_stock_marquetEx->stock_operations, _operation);
@@ -157,3 +140,19 @@ void* operation_executer(void* args){
         }
 }
 
+
+// initialization of the mutex and condition variables
+void init_concurrency_mechanisms(){
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
+    pthread_cond_init(&cond2, NULL);
+    printf("------>init concurrency mechanisns ok \n");
+}
+
+// Deallocate memory for the mutex and condition variables 
+void destroy_concurrency_mechanisms(){
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
+    pthread_cond_destroy(&cond2);
+    printf("------>destroy concurrency mechanisns ok \n");
+}
